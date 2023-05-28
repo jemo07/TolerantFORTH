59a5955038e35618a0c0301f18516791f9b2fd3d
jemo07 <32791115+jemo07@users.noreply.github.com>
2023-05-28 19:55:09 +0200

    Update startup.s

--- src/startup.s
+++ src/startup.s
@@ -1,11 +1,28 @@
 .section .text
 .global _start
 
+// Interrupt Vector Table
+.section .vector_table
+.global _vector_table
+
+_vector_table:
+  // Reset vector
+  .word _reset_vector
+
+  // Interrupt Service Routines (ISRs)
+  .word _timer_isr
+  .word _external_isr
+  .word _exception_isr
+
+.section .text
 _reset_vector:
   b _start
 
 _start:
   // Perform any necessary initialization tasks
+
+  // Initialize the stack pointer (SP) to a valid memory address
+  ldr sp, =_stack_top
 
   // Call the entry point of your TolerantForth system
   bl main
@@ -16,4 +33,7 @@ _start:
   b .
 
 .section .bss
-// Define any uninitialized data (if needed)
+.global _stack_top
+_stack_top:
+  // Define the top of the stack memory address
+  // Adjust the value to suit your memory layout and stack requirements
