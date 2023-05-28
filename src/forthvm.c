797925f6ccdeb6f43f916449ccc060936c33c490
jemo07 <32791115+jemo07@users.noreply.github.com>
2023-05-29 01:01:10 +0200

    Update forthvm.c

--- src/forthvm.c
+++ src/forthvm.c
@@ -17,6 +17,36 @@ int32_t thread = 0;
 int32_t data[8192];
 unsigned char cData[32768];
 
+
+// Parser functions 
+void parse_input() {
+    char input[256];
+
+    // Read user input
+    fgets(input, sizeof(input), stdin);
+
+    // Tokenize input into words
+    char* token = strtok(input, " \t\n");
+
+    while (token != NULL) {
+        // Find word in dictionary
+        forthvm_word* word = forthvm_find_word(token);
+
+        if (word != NULL) {
+            // Execute word
+            forthvm_execute(word->token);
+        } else {
+            // Word not found in dictionary
+            printf("Unknown word: %s\n", token);
+        }
+
+        // Get next token
+        token = strtok(NULL, " \t\n");
+    }
+}
+
+// 
+
 // Helper functions for terminal I/O
 int readNumber() {
     int number;
@@ -34,6 +64,16 @@ void printString(const char* str) {
 
 void printChar(char c) {
     printf("%c", c);
+}
+void qrx(void) {
+    forthvm_push(readNumber());
+    if (top != 0)
+        forthvm_push(TRUE);
+}
+
+void txsto(void) {
+    printChar(top);
+    forthvm_pop();
 }
 
 // Core Forth words
@@ -289,6 +329,95 @@ void ABORQ(const char seq[]) {
     }
 }
 
+void dup2(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    forthvm_push(value2);
+    forthvm_push(value1);
+    forthvm_push(value2);
+    forthvm_push(value1);
+}
+
+void swap2(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    forthvm_push(value1);
+    forthvm_push(value2);
+}
+
+void drop2(void) {
+    forthvm_drop();
+    forthvm_drop();
+}
+
+void over2(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    forthvm_push(value2);
+    forthvm_push(value1);
+    forthvm_push(value2);
+}
+
+void pick2(void) {
+    int index = forthvm_pop();
+    int value1 = forthvm_stack(index);
+    int value2 = forthvm_stack(index - 1);
+    forthvm_push(value2);
+    forthvm_push(value1);
+}
+
+void rot2(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    int value3 = forthvm_pop();
+    forthvm_push(value2);
+    forthvm_push(value1);
+    forthvm_push(value3);
+}
+
+void minus(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    forthvm_push(value2 - value1);
+}
+
+void negate(void) {
+    int value = forthvm_pop();
+    forthvm_push(-value);
+}
+
+void max2(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    forthvm_push((value1 > value2) ? value1 : value2);
+}
+
+void min2(void) {
+    int value1 = forthvm_pop();
+    int value2 = forthvm_pop();
+    forthvm_push((value1 < value2) ? value1 : value2);
+}
+
+// Supporting IO Functions     
+void forthvm_word_print() {
+    printNumber(forthvm_pop());
+}
+
+void forthvm_word_println() {
+    printNumber(forthvm_pop());
+    printChar('\n');
+}
+
+void forthvm_word_emit() {
+    printChar(forthvm_pop());
+}
+
+void forthvm_define_io_words() {
+    forthvm_define_word("PRINT", forthvm_word_print);
+    forthvm_define_word("PRINTLN", forthvm_word_println);
+    forthvm_define_word("EMIT", forthvm_word_emit);
+}
+
 void forthvm_init() {
     // Initialize stack and memory
     top = 0;
@@ -300,7 +429,12 @@ void forthvm_init() {
     for (int i = 0; i < MAX_DICTIONARY_SIZE; i++) {
         dictionary[i].name = NULL;
         dictionary[i].func = NULL;
+    // Initialize IO fuctions
+    forthvm_define_io_words();
+    // Call forthvm_define_additional_words() 
+    forthvm_define_additional_words();    
     }
+
 
     // Define built-in words
     forthvm_define_word("BYE", bye);
@@ -354,6 +488,17 @@ void forthvm_init() {
     forthvm_define_word("CMOVE", cmove);
     forthvm_define_word("MOVE", move);
     forthvm_define_word("FILL", fill);
+    forthvm_define_word("DUP2", dup2);
+    forthvm_define_word("SWAP2", swap2);
+    forthvm_define_word("DROP2", drop2);
+    forthvm_define_word("OVER2", over2);
+    forthvm_define_word("PICK2", pick2);
+    forthvm_define_word("ROT2", rot2);
+    forthvm_define_word("MINUS", minus);
+    forthvm_define_word("NEGATE", negate);
+    forthvm_define_word("MAX2", max2);
+    forthvm_define_word("MIN2", min2);
+}
 }
 
 void forthvm_run(const char* input) {
