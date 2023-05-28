8b6c37cf7042960cd8b9489de2552751c46d05c5
jemo07 <32791115+jemo07@users.noreply.github.com>
2023-05-28 20:31:29 +0200

    Update forthvm.c

--- src/forthvm.c
+++ src/forthvm.c
@@ -1,1 +1,77 @@
+#include "forthvm.h"
+
+// Data structures
+struct Dictionary {
+    const char *name;
+    void (*func)();
+};
+
+// Global variables
+static int stack[MAX_STACK_SIZE];
+static int stack_pointer = -1;
+static Dictionary dictionary[MAX_DICTIONARY_SIZE];
+static int dictionary_size = 0;
+
+// Helper functions
+static void push(int value) {
+    if (stack_pointer < MAX_STACK_SIZE - 1) {
+        stack[++stack_pointer] = value;
+    } else {
+        // Handle stack overflow error
+        // (e.g., print an error message, terminate the program, etc.)
+    }
+}
+
+static int pop() {
+    if (stack_pointer >= 0) {
+        return stack[stack_pointer--];
+    } else {
+        // Handle stack underflow error
+        // (e.g., print an error message, terminate the program, etc.)
+        return 0; // Return a default value
+    }
+}
+
+// Forth VM functions
+void forthvm_init() {
+    stack_pointer = -1;
+    dictionary_size = 0;
+}
+
+void forthvm_run() {
+    // Enter the Forth interpreter loop
+    while (1) {
+        // TODO: Read input and parse Forth tokens
+
+        // TODO: Execute the parsed Forth tokens using forthvm_execute()
+    }
+}
+
+void forthvm_push(int value) {
+    push(value);
+}
+
+int forthvm_pop() {
+    return pop();
+}
+
+void forthvm_execute(int token) {
+    // TODO: Implement the execution of Forth tokens
+    // based on the defined words in the dictionary
+}
+
+void forthvm_define_word(const char *name, void (*func)()) {
+    if (dictionary_size < MAX_DICTIONARY_SIZE) {
+        dictionary[dictionary_size].name = name;
+        dictionary[dictionary_size].func = func;
+        dictionary_size++;
+    } else {
+        // Handle dictionary overflow error
+        // (e.g., print an error message, terminate the program, etc.)
+    }
+}
+
+void forthvm_reset() {
+    forthvm_init();
+}
 
