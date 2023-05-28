b5507b693901f14fa68c1da3eebe589c8fbf57b2
jemo07 <32791115+jemo07@users.noreply.github.com>
2023-05-28 20:13:27 +0200

    Update startup.s

--- src/startup.s
+++ src/startup.s
@@ -1,39 +1,62 @@
 .section .text
 .global _start
 
-// Interrupt Vector Table
+/* Reset Vector */
+_reset_vector:
+  b _start
+
+/* Interrupt Vector Table */
 .section .vector_table
 .global _vector_table
 
 _vector_table:
-  // Reset vector
+  /* Reset vector */
   .word _reset_vector
 
-  // Interrupt Service Routines (ISRs)
+  /* Interrupt Service Routines (ISRs) */
   .word _timer_isr
   .word _external_isr
   .word _exception_isr
 
 .section .text
-_reset_vector:
-  b _start
-
 _start:
-  // Perform any necessary initialization tasks
+  /* Set the appropriate processor mode and execution level */
+  mrs x0, CurrentEL
+  and x0, x0, #0xC
+  cmp x0, #0xC
+  b.eq skip_el_change
+  mov x0, #0xC
+  msr DAIFSet, x0
 
-  // Initialize the stack pointer (SP) to a valid memory address
+skip_el_change:
+  /* Initialize the stack pointer (SP) to a valid memory address */
   ldr sp, =_stack_top
 
-  // Call the entry point of your TolerantForth system
+  /* Memory Initialization */
+  /* Perform any necessary initialization tasks for memory configuration and MMU */
+
+  /* Peripheral Initialization */
+  /* Perform initialization and configuration of peripherals */
+
+  /* Enable Cache and Memory Subsystem */
+  /* Configure cache subsystem and memory hierarchy if applicable */
+
+  /* Enable Core Features */
+  /* Enable any additional core features required */
+
+  /* Call TolerantForth Entry Point */
   bl main
 
-  // Handle system shutdown or other actions if needed
+  /* Handle system shutdown or other actions if needed */
 
-  // Infinite loop to prevent program execution from falling through
+  /* Infinite loop to prevent program execution from falling through */
   b .
 
-.section .bss
+.section .data
 .global _stack_top
 _stack_top:
-  // Define the top of the stack memory address
-  // Adjust the value to suit your memory layout and stack requirements
+  /* Define the top of the stack memory address */
+  /* Adjust the value to suit your memory layout and stack requirements */
+
+.section .bss
+/* Define any uninitialized data (if needed) */
