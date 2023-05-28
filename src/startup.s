71439dbe082ce2d86c6e3b7a60b514d6fd76b449
jemo07 <32791115+jemo07@users.noreply.github.com>
2023-05-28 20:23:16 +0200

    Update startup.s

--- src/startup.s
+++ src/startup.s
@@ -1,62 +1,102 @@
-.section .text
-.global _start
+..section .text.boot
 
 /* Reset Vector */
+.globl _reset_vector
 _reset_vector:
-  b _start
+  ldr pc, _start
 
 /* Interrupt Vector Table */
 .section .vector_table
-.global _vector_table
+.globl _vector_table
 
 _vector_table:
   /* Reset vector */
   .word _reset_vector
-
-  /* Interrupt Service Routines (ISRs) */
-  .word _timer_isr
-  .word _external_isr
-  .word _exception_isr
+  /* Add other interrupt vectors as needed */
 
 .section .text
+
+/* Processor Modes and Execution Levels */
+.equ ARM_MODE_USR, 0x10
+.equ ARM_MODE_FIQ, 0x11
+.equ ARM_MODE_IRQ, 0x12
+.equ ARM_MODE_SVC, 0x13
+.equ ARM_MODE_HYP, 0x1A
+.equ ARM_MODE_UND, 0x1B
+.equ ARM_MODE_SYS, 0x1F
+
+.equ ARM_MODE_MASK, 0x1F
+.equ ARM_I_BIT, 0x80
+.equ ARM_F_BIT, 0x40
+
+.equ CPSR_MODE_USER, 0x10
+.equ CPSR_MODE_FIQ, 0x11
+.equ CPSR_MODE_IRQ, 0x12
+.equ CPSR_MODE_SVC, 0x13
+.equ CPSR_MODE_ABORT, 0x17
+.equ CPSR_MODE_UNDEFINED, 0x1B
+.equ CPSR_MODE_SYSTEM, 0x1F
+
+.equ CPSR_IRQ_INHIBIT, 0x80
+.equ CPSR_FIQ_INHIBIT, 0x40
+.equ CPSR_THUMB, 0x20
+
+/* Stack Pointer Initialization */
+.globl _get_stack_pointer
+_get_stack_pointer:
+  str sp, [sp]
+  ldr r0, [sp]
+  mov pc, lr
+
+/* Memory Initialization */
+/* Add your memory initialization code here */
+
+/* _start label */
+.globl _start
 _start:
   /* Set the appropriate processor mode and execution level */
-  mrs x0, CurrentEL
-  and x0, x0, #0xC
-  cmp x0, #0xC
-  b.eq skip_el_change
-  mov x0, #0xC
-  msr DAIFSet, x0
+  mrs r0, CPSR
+  orr r0, r0, #(ARM_I_BIT | ARM_F_BIT)
+  and r11, r0, #ARM_MODE_MASK
+  cmp r11, #ARM_MODE_HYP
+  bne .NotInHypMode
+  bic r0, r0, #ARM_MODE_MASK
+  orr r0, r0, #ARM_MODE_SVC
+  msr spsr_cxsf, r0
+  add lr, pc, #4
+  msr ELR_hyp, lr
+  eret
 
-skip_el_change:
-  /* Initialize the stack pointer (SP) to a valid memory address */
-  ldr sp, =_stack_top
+.NotInHypMode:
+  mov r0, #0x8000
+  mov r1, #0x0000
+  ldmia r0!, {r2-r9}
+  stmia r1!, {r2-r9}
+  ldmia r0!, {r2-r9}
+  stmia r1!, {r2-r9}
 
-  /* Memory Initialization */
-  /* Perform any necessary initialization tasks for memory configuration and MMU */
+  mov r0, #0xD2
+  msr cpsr_c, r0
+  ldr sp, =__irq_stack_base
 
-  /* Peripheral Initialization */
-  /* Perform initialization and configuration of peripherals */
+  mov r0, #0xD3
+  msr cpsr_c, r0
+  ldr sp, =__svc_stack_base
 
   /* Enable Cache and Memory Subsystem */
-  /* Configure cache subsystem and memory hierarchy if applicable */
+  /* Add cache and memory subsystem configuration here */
 
   /* Enable Core Features */
-  /* Enable any additional core features required */
+  /* Add any necessary core feature enablement here */
 
   /* Call TolerantForth Entry Point */
   bl main
 
-  /* Handle system shutdown or other actions if needed */
+/* Interrupt Service Routines (ISRs) */
+/* Add your ISR implementations here */
 
-  /* Infinite loop to prevent program execution from falling through */
-  b .
+/* Halt */
+_inf_loop:
+  b _inf_loop
 
-.section .data
-.global _stack_top
-_stack_top:
-  /* Define the top of the stack memory address */
-  /* Adjust the value to suit your memory layout and stack requirements */
-
-.section .bss
-/* Define any uninitialized data (if needed) */
+.section .
