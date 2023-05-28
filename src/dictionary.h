64eee646e1bd50bf9c845e76b33b59e5d20bc2c5
jemo07 <32791115+jemo07@users.noreply.github.com>
2023-05-29 01:20:22 +0200

    Update dictionary.h

--- src/dictionary.h
+++ src/dictionary.h
@@ -1,1 +1,14 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+// Define the maximum length of a word
+#define MAX_WORD_LENGTH 32
+
+// Function to add a new word to the dictionary
+void dictionary_add(const char* word, void (*code)());
+
+// Function to find the code associated with a word in the dictionary
+void (*dictionary_find(const char* word))();
+
+#endif /* DICTIONARY_H */
 
